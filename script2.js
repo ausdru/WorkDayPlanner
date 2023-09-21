e5593f67ac7616e04d342ec61a9f977026b9cfc9
Austin <137249651+ausdru@users.noreply.github.com>
2023-09-21 17:04:22 -0400

    Finalizing project for deployment.

---
+++ script2.js
@@ -0,0 +1,90 @@
+// Displaying current time & date in header section.
+// --------------------------------------------------------------------------
+var currentTimeDisplay = $("#currentDay");
+currentTimeDisplay.text(moment().format('LLLL'));
+
+// Creating variables for text-area to be utilized & retrieve value.
+// --------------------------------------------------------------------------
+var hrNineText = $("#hour-9");
+var hrTenText = $("#hour-10");
+var hrElevenText = $("#hour-11");
+var hrTwelveText = $("#hour-12");
+var hrThirteenText = $("#hour-13");
+var hrFourteenText = $("#hour-14");
+var hrFifteenText = $("#hour-15");
+var hrSixteenText = $("#hour-16");
+var hrSeventeenText = $("#hour-17");
+var confirmationMsg = $("#confirmation-msg");
+var timeBlock = $(".time-block")
+
+// Save button to initialize a click event to store text-area value to
+// local storage.
+// --------------------------------------------------------------------------
+var saveBtn = $(".saveBtn");
+
+// On the click, save the value of all items to local storage.
+// --------------------------------------------------------------------------
+saveBtn.on("click", function(e) {
+    e.preventDefault();
+    confirmationMsg.html("Successfully added to planner with <code> localStorage </code>!");
+    confirmationMsg.attr("style", "text-align: center; font-size: 15px");
+    hrNineVal = hrNineText.val();
+    hrTenVal = hrTenText.val();
+    hrElevenVal = hrElevenText.val();
+    hrTwelveVal =  hrTwelveText.val();
+    hrThirteenVal = hrThirteenText.val(); 
+    hrFourteenVal = hrFourteenText.val(); 
+    hrFifteenVal = hrFifteenText.val();
+    hrSixteenVal = hrSixteenText.val();
+    hrSeventeenVal = hrSeventeenText.val();
+    localStorage.setItem("hour09", hourNineVal);
+    localStorage.setItem("hour10", hourTenVal);
+    localStorage.setItem("hour11", hourElevenVal);
+    localStorage.setItem("hour12", hourTwelveVal);
+    localStorage.setItem("hour13", hourThirteenVal);
+    localStorage.setItem("hour14", hourFourteenVal);
+    localStorage.setItem("hour15", hourFifteenVal);
+    localStorage.setItem("hour16", hourSixteenVal);
+    localStorage.setItem("hour17", hourSeventeenVal);
+});
+
+// When page loads, place all items in local storage to corresponding elements
+// on the page.
+// --------------------------------------------------------------------------
+function init() {
+    hrNineText.text(localStorage.getItem("hour09"));
+    hrTenText.text(localStorage.getItem("hour10"));
+    hrElevenText.text(localStorage.getItem("hour11"));
+    hrTwelveText.text(localStorage.getItem("hour12"));
+    hrThirteenText.text(localStorage.getItem("hour13"));
+    hrFourteenText.text(localStorage.getItem("hour14"));
+    hrFifteenText.text(localStorage.getItem("hour15"));
+    hrSixteenText.text(localStorage.getItem("hour16"));
+    hrSeventeenText.text(localStorage.getItem("hour17"));
+};
+
+init()
+
+// Finding value for the current hour.
+// --------------------------------------------------------------------------
+var currentHr = moment().hours();
+
+// Comparing current hour to planner's hour to give correct color.
+// --------------------------------------------------------------------------
+for (var i = 0; i < timeBlock.length; i++) {
+    if (timeBlock[i].dataset.time == currentHr) {
+        timeBlock[i].classList.remove("past");
+        timeBlock[i].classList.remove("future");
+        timeBlock[i].classList.add("present");
+    };
+    if (timeBlock[i].dataset.time > currentHr) {
+        timeBlock[i].classList.remove("past");
+        timeBlock[i].classList.remove("present");
+        timeBlock[i].classList.add("future");
+    };
+    if (timeBlock[i].dataset.time < currentHr) {
+        timeBlock[i].classList.remove("present");
+        timeBlock[i].classList.remove("future");
+        timeBlock[i].classList.add("past");
+    }
+};
